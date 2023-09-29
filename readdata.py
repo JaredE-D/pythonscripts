d35694dc6d69e09debadd4e1be8bf6835c0d9b49
gwarbruff <77998737+gwarbruff@users.noreply.github.com>
2023-09-28 20:34:58 -0700

    Create readdata.py

---
+++ readdata.py
@@ -0,0 +1,6 @@
+#We want to take .mat files of multiple runs, combine them and then graph all of them/ averages of them.
+#How we do this is first we create a super array of mat files.
+#One for the original values and one for the updated time/pos
+
+import scipy.io as sio
+import numpy as np
